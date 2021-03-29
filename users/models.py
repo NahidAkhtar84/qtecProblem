caa58e22e8b150e39a0254b8af8e81fd19929761
NahidAkhtar84 <nahid.ibne.akhtar@gmail.com>
2021-03-29 17:41:58 +0600

    user field refactored

--- users/models.py
+++ users/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class User(models.Model):
-    user_name = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=20, default='0')
+    user_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255, default='0')
     email = models.EmailField()
-    password = models.CharField(max_length=40)
+    password = models.CharField(max_length=255)
 
     def __str__(self):
         return self.user_name
